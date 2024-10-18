d3d59682b8e5f172d2540067078b052cca4a0859
JOSEonSOLANA <joseonsolana@gmail.com>
2024-10-18 12:46:43 +0200

    se actualiza css

--- src/counter.ts
+++ src/counter.ts
@@ -8,15 +8,7 @@ export function setupCounter(element: HTMLButtonElement) {
   setCounter(0)
 }
 
-export function restarCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `El contador es ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter - 2))
-  setCounter(0)
-}
+
 
 
 
